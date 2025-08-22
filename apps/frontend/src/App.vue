3248f1e8e34178a29644299853d817b23f4c9b4a
Gil Zeevi <gilsmol91@gmail.com>
2025-08-23 00:07:35 +0300

    chore: scaffold backend and frontend apps

---
+++ apps/frontend/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>IP to Country</h1>
+    <IpSearch />
+  </div>
+</template>
+
+<script setup>
+import IpSearch from './components/IpSearch.vue'
+</script>
---
+++ apps/frontend/src/components/IpSearch.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <input v-model="ip" placeholder="Enter IP" />
+    <button @click="search">Search</button>
+    <p v-if="country">Country: {{ country }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const ip = ref('')
+const country = ref('')
+
+function search() {
+  country.value = 'Unknown'
+}
+</script>
